90ad800c0c2ee90966064b651c3057c23923d601
atapley <tapleyalx@gmail.com>
2021-02-09 15:03:24 -0500

    Added connection to AWS RDS and posted to API endpoint

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
 from typing import Optional
-
+import mysql.connector
 from fastapi import FastAPI
+import json
+
+mydb = mysql.connector.connect(
+    host="deeplift-database.cugzfo4s5ndw.us-east-1.rds.amazonaws.com",
+    user="deeplifter123",
+    password="j4capstone2021",
+    port="3306",
+    database="deeplift"
+)
 
 app = FastAPI()
 
@@ -9,6 +18,15 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/users")
+def read_item():
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM DeepliftUser")
+    myresult = mycursor.fetchall()
+    res = []
+    for user in myresult:
+        res.append(user)
+    return res
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
